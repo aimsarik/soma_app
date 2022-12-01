539d5c57110e75a16c608e13fe7cea83fef221ed
aimsarik <aim_sarik@yahoo.com>
2022-12-02 00:52:59 +0700

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require("electron-updater");
+const isDev = require("electron-is-dev");
+const ProgressBar = require('electron-progressbar');
+const path = require('path');
 const { SerialPort } = require('serialport');
 const port = new SerialPort({ path: 'COM4', baudRate: 9600 });
 
@@ -14,20 +17,46 @@ function createWindow () {
       nodeIntegration: true,
       // devTools: false,
     }
-  })
-
-  win.loadURL("http://chamnan.local/front_end/gym/");
+  })    
+  win.loadURL("http://localhost/");
   win.setIcon(path.join(__dirname, "/src/assets/home_screen.png"));
+  
+  
+  if (!isDev) {
+		autoUpdater.checkForUpdates();
+	};
+
   win.maximize();
   win.webContents.openDevTools();
 
   ipcMain.on("open_door", (event, arg) => {
     port.write("1");
   });
+  // wait and show progress bar
+  setTimeout(() => {
+    var progressBar = new ProgressBar({
+      text: 'កំពុងទាញទិន្នន័យ...',
+      detail: 'សូមរង់ចាំ...'
+    });
+    progressBar
+      .on('completed', function() {
+        console.info(`completed...`);
+        progressBar.detail = 'Task completed. Exiting...';
+      })
+      .on('aborted', function() {
+        console.info(`aborted...`);
+      });
+      win.webContents.on('did-finish-load',WindowsReady);
+
+      function WindowsReady() {
+          console.log('Ready');
+          progressBar.setCompleted();
+      }      
+  }, 500);
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -41,3 +70,42 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+autoUpdater.on("checking-for-update", () => {
+});
+autoUpdater.on("update-available", () => {
+  // progresbar with percentage
+  var progressBarPer = new ProgressBar({
+    indeterminate: false,
+    text: 'Preparing data...',
+    detail: 'Wait...'
+  });
+
+  progressBarPer
+    .on('completed', function() {
+      console.info(`completed...`);
+      progressBarPer.detail = 'Task completed. Exiting...';
+    })
+    .on('aborted', function(value) {
+      console.info(`aborted... ${value}`);
+    })
+    .on('progress', function(value) {
+      progressBarPer.detail = `Value ${value} out of ${progressBarPer.getOptions().maxValue}...`;
+    });
+
+  autoUpdater.on("download-progress", (processTrack) => {
+    if (processTrack.percent > 0) {
+      progressBarPer.value = processTrack.percent;
+    }
+  });
+});
+
+autoUpdater.on("update-not-available", () => {
+});
+
+autoUpdater.on("error", (error) => {
+});
+
+autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+  // restart the app and install the update
+  autoUpdater.quitAndInstall();
+});
